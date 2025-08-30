df02393845077e8c012daadf6f404282d1f70cee
Zain Ali <zauhqm6@gmail.com>
2025-08-30 17:44:15 +0500

    Update Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -40,7 +40,7 @@ import { FaUsers, FaAccessibleIcon, FaRegFileAlt } from "react-icons/fa";
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      {/* Core Front-End */}
+      {/* Core Front */}
       <Col xs={4} md={2} className="tech-icons"><DiHtml5 /></Col>
       <Col xs={4} md={2} className="tech-icons"><DiCss3 /></Col>
       <Col xs={4} md={2} className="tech-icons"><DiJavascript1 /></Col>
