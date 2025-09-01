9fda19c9a4fde157250caca29f3dba63122ac779
Zain Ali <zauhqm6@gmail.com>
2025-09-01 14:29:31 +0500

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,5 +1,5 @@
 // import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container} from "react-bootstrap";
 // import myImg from "../../Assets/avatar.svg";
 // import Tilt from "react-parallax-tilt";
 // import {
